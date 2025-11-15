9e0f894fcfa60cf91442fdc856ad19ba8aa7f201
sankarviacode <sivaaadi96@gmail.com>
2025-11-15 15:58:09 +0530

    logo changes

--- middlewares/createtoken.js
+++ middlewares/createtoken.js
@@ -2,14 +2,14 @@ import jwt from "jsonwebtoken";
 
 const createTokenandCookies = (req, res, user) => {
   const token = jwt.sign(user, process.env.JWT_KEY, {
-    expiresIn: "7d",
+    expiresIn: "10y",
   });
   res.cookie("token", token, {
     httpOnly: true,
     secure: true, // must be true for HTTPS (Render)
     sameSite: "none", // required for cross-site requests
     path: "/", // makes it available everywhere
-    maxAge: 365 * 24 * 60 * 60 * 1000, // 7 days
+   maxAge: 10 * 365 * 24 * 60 * 60 * 1000, // 10 years
   });
 
   return token;
